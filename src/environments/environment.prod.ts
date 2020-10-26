eeded1a74dc896fc8b98120bb5526831577aa854
Benjamin <patonacevedo>
2020-10-26 13:04:45 -0400

    Added variables to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAaVgvtFxivNr2YR06RpakeVq0K8xTbxrQ',
+    authDomain: 'localbrands-966b6.firebaseio.com',
+    databaseURL: 'https://localbrands-966b6.firebaseio.com',
+    projectId: 'localbrands-966b6',
+    storageBucket: 'localbrands-966b6.appspot.com',
+  }
