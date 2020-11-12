e8ba0077ff7106d6d7add203868272e0b957b4b5
Ben Acevedo <ben.acevedo@solarwinds.com>
2020-11-12 02:09:04 -0500

    added return type

--- src/app/store/user/user.selector.ts
+++ src/app/store/user/user.selector.ts
@@ -6,7 +6,7 @@ export const userFeaturesSelector = createFeatureSelector<IUserState>('user');
 
 export const favShopsSelector$ = createSelector(
   userFeaturesSelector,
-  (state: IUserState) => state.user.fav_stores,
+  (state: IUserState): Set<string> => state.user.fav_stores,
 );
 
 export const userDetailsSelector = createSelector(
