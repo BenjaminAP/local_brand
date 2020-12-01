7108c587db74abf6a86656b8b12b5ed8b0e185fa
Benjamin Acevedo <patonacevedo@gmail.com>
2020-12-01 14:20:29 -0500

    started filter 2 model

--- src/app/models/i.filter.ts
+++ src/app/models/i.filter.ts
@@ -3,3 +3,12 @@ export interface IFilter {
   type: string;
   active: boolean;
 }
+
+export interface IFilter2 {
+  store_type: string[];
+  attire_type: string[];
+  country: string[];
+  state: string[];
+  city: string[];
+}
+
